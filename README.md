c617a87529506e546abfc5ff43b7432a1fd492cf
VPeti1 <vpeti011@outlook.com>
2024-03-05 20:24:36 +0100

    Updated installer

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 An all in one package manager for linux
 
 # How to install
-## Download the latest installer
+## Download the installer.out file
 ## Type in "chmod +x installer.out"
 ## Type in "./installer.out"
 ## AIOPM is now added to PATH and you can run it by typing in 'aiopm' on your terminal
--- installer.cpp
+++ installer.cpp
@@ -9,18 +9,35 @@
 #define clear std::cout << "\033[2J\033[1;1H";
 using namespace std;
 
+
+void dw() {
+    std::string distro;
+    std::cout << "Enter your Linux distribution (arch/debian/fedora): ";
+    std::cin >> distro;
+    if (distro == "arch") {
+        system("sudo pacman -S git wget");
+    } else if (distro == "debian") {
+        system("sudo apt-get install git wget");
+    } else if (distro == "fedora") {
+        system("sudo dnf install git wget");
+    }
+    else if (distro == "skip") {
+
+    }
+     else {
+        std::cout << "Unsupported distribution. Please choose arch, debian, or fedora." << std::endl;
+    }
+}
+
 int main() {
     clear;
     std::cout << "AIOPM Installer" << std::endl;
     std::cout << "By VPeti" << std::endl;
     sleep(2);
-    std::cout << "Make sure you have WGET installed!" << std::endl;
+    dw();
     system("read -p 'Press Enter to continue...'");
-    system("sudo mkdir /usr/aiopmi");
-    system("sudo wget https://raw.githubusercontent.com/VPeti1/CWAcces/main/AIOPM.out -O /usr/aiopmi/AIOPM.out");
-    system("sudo cp -r /usr/aiopmi/AIOPM.out /bin/aiopm");
+    system("sudo wget https://raw.githubusercontent.com/VPeti1/CWAcces/main/AIOPM.out -O /bin/aiopm");
     system("sudo chmod +x /bin/aiopm");
     std::cout << "AIOPM Installer Completed!" << std::endl;
-    system("read -p 'Press Enter to continue...'");
     system("exit");
 }
